cf29ab70b6210938419ea25301f5610de1797af8
joedavidson00 <jdavidso@highpoint.edu>
2019-01-28 13:13:29 -0500

    After mondays class pt. 2

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//page 24
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
@@ -27,11 +28,25 @@ class ViewController: UIViewController {
     var currentQuestionIndex: Int = 0
     
     @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "????"
         
     }
 
     @IBAction func showAnswer(_ sender: UIButton) {
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
     }
 
 }
