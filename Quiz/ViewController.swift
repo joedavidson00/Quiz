f8f8405c4df7764fa549785c89452b65329e3af9
Joe Davidson <jdavidso@highpoint.edu>
2019-01-29 15:25:27 -0500

    added some more comments just so my code is well documented

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -13,21 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    
+    // the list for questions
     let questions: [String] = [
         "What is 7+7?",
         "What is the capital of Vermont?",
         "What is cognac made from?"
     ]
     
+    // the list with corresponding answers
     let answers: [String] = [
         "14",
         "Montpelier",
         "Grapes"
     ]
     
+    //use this to change the question and answer values
     var currentQuestionIndex: Int = 0
     
     @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+        // this moves to the next question
         currentQuestionIndex += 1
         
         if currentQuestionIndex == questions.count {
@@ -40,6 +46,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAnswer(_ sender: UIButton) {
+        // this sets the answer to the answer corresponding with the question (index value)
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
     }
